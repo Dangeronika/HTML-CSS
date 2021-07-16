a9c1271a98aa8dc56c846ac0815e74bafa6dd096
Dangeronika <Panda99230401@yandex.ru>
2021-07-16 22:03:46 +0300

    Test ajax

---
+++ js/ajax.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+ 
+    // Check for hash value in URL
+    // var hash = window.location.hash.substr(1);
+    // var href = $('#headernav div a').each(function(){
+    //     var href = $(this).attr('href');
+    //     if(hash==href.substr(0,href.length-5)){
+    //         var toLoad = hash+'.html #content';
+    //         $('#content').load(toLoad)
+    //     } 
+    // });
+    
+    $('#headernav div a').click(function(){
+    
+    var toLoad = $(this).attr('href')+' #content';
+    $('#content').hide('fast',loadContent);
+    window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+    function loadContent() {
+     $('#content').load(toLoad,'',showNewContent())
+    }
+    function showNewContent() {
+     $('#content').show('normal',hideLoader());
+    }
+    function hideLoader() {
+     $('#load').fadeOut('normal');
+    }
+    return false;
+    
+    });
+});
