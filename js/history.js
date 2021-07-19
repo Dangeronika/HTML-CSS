520e3cef28362d776d96b8c7d98f676543d20e17
Dangeronika <Panda99230401@yandex.ru>
2021-07-19 20:31:50 +0300

    Add a request for ajax works (test)

---
+++ js/history.js
@@ -0,0 +1,48 @@
+function showContent(link) {
+  var cont = document.getElementById('content'); 
+  var http = createRequestObject();
+  if( http ) 
+  { http.open('get', link);
+      http.onreadystatechange = function () 
+      {   if(http.readyState == 4) 
+          {   cont = pageChange(link);
+        }
+      }
+      http.send(null);  }
+  else 
+  {  document.location = link;   }   }
+// ajax объект
+function createRequestObject() 
+{ return new XMLHttpRequest() }
+
+function pageChange (link) {
+  
+  // let toLoad = $(this).attr('href')+' #content';
+  // $('#content').hide('fast',loadContent);
+  // window.location.hash = link.replace('.html', '');
+  // function loadContent() {
+  // $('#content').load(link,'',showNewContent())
+  // }
+  // function showNewContent() {
+  //   $('#content').show('normal',hideLoader());
+  //  }
+  // function hideLoader() {
+  //   $('#load').fadeOut('normal');
+  // }
+  let toLoad = link + ' #content';
+    $('#content').hide('fast',loadContent);
+    window.location.hash = link.replace('.html', '');
+    function loadContent() {
+     $('#content').load(toLoad,'',showNewContent())
+    }
+    function showNewContent() {
+     $('#content').show('normal',hideLoader());
+    }
+    function hideLoader() {
+     $('#load').fadeOut('normal');
+    }
+}
+
+$( document ).ajaxComplete(function() {
+  initMap();
+});
