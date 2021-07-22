3e6bcf65700f76e46424f54cef1b7123ba7826ea
Dangeronika <Panda99230401@yandex.ru>
2021-07-22 21:20:17 +0300

    Updated selected pages indexOf replaced on includes

--- js/history.js
+++ js/history.js
@@ -1,28 +1,44 @@
 $(document).ready(function() {
 
+  changeStyle();
   $('#headernav div a').click(function(e) {
       e.preventDefault();
-      changeStyle($(this));
       href = $(this).attr('href');
       let toLoad = href+' #content';
       $('#content').hide('fast', () => loadContent(toLoad));
       history.pushState(null, null, href);
+      changeStyle();
   })
 })
 
-function changeStyle(obj) {
-  $('#headernav div a').css({'color' : 'black', 'padding-top' : ''});
+function changeStyle() {
+  $('#headernav div a').css({'color' : '', 'padding-top' : ''});
   $('.animationdiv').css({'border-bottom' : ''});
-  obj.css({'color' : '#2375E1', 'padding-top' : '3px'});
-  obj.parent().css({ 'border-bottom' : '3px solid #2375E1'});
+  selecter = document.location.pathname;
+
+  if (selecter.includes('index')) {
+    $('.index_href').css({'color' : '#2375E1', 'padding-top' : '3px'});
+    $('.index_href').parent().css({ 'border-bottom' : '3px solid #2375E1'});
+  }
+
+  if (selecter.includes('Map')) {
+    $('.Map_href').css({'color' : '#2375E1', 'padding-top' : '3px'});
+    $('.Map_href').parent().css({ 'border-bottom' : '3px solid #2375E1'});
+  }
+
+  if (selecter.includes('Timer')) {
+    $('.Timer_href').css({'color' : '#2375E1', 'padding-top' : '3px'});
+    $('.Timer_href').parent().css({ 'border-bottom' : '3px solid #2375E1'});
+  }
 }
 
 function loadContent( road ) {
   $('#content').load( road, '', () => showNewContent())
 }
 function showNewContent() {
+  changeStyle();
   $('#content').show('normal');
-  if(document.location.pathname.indexOf('Map.html')) {
+  if(document.location.pathname.includes('Map.html')) {
     initMap();
   }
 }
