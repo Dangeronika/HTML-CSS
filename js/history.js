4d97a7b5eaccb25ca1d7b52853ac23ccd42790ab
Dangeronika <Panda99230401@yandex.ru>
2021-07-22 14:24:18 +0300

    Rework with help from God request+ spa+ timer+ map+ ajax+

--- js/history.js
+++ js/history.js
@@ -1,36 +1,28 @@
-function showContent(link) {
-  var cont = document.getElementById('content'); 
-  var http = createRequestObject();
-  if( http ) 
-  { http.open('get', link);
-      http.onreadystatechange = function () 
-      {   if(http.readyState == 4) 
-          {   cont = pageChange(link);
-        }
+$(document).ready(function() {
+
+  $('#headernav div a').click(function(e){
+      e.preventDefault();
+      changeStyle($(this));
+      href = $(this).attr('href');
+      let toLoad = href+' #content';
+      $('#content').hide('fast',loadContent);
+      history.pushState(null, null, href);
+      function loadContent() {
+      $('#content').load(toLoad,'', () => showNewContent())
       }
-      http.send(null);  }
-  else 
-  {  document.location = link;   }   }
-// ajax объект
-function createRequestObject() 
-{ return new XMLHttpRequest() }
+      function showNewContent() {
+      $('#content').show('normal',hideLoader());
+      if (href == "Map.html") { initMap(); }
+      }
+      function hideLoader() {
+      $('#load').fadeOut('normal');
+      }
+  })
+})
 
-function pageChange (link) {
-  
-  let toLoad = link + ' #content';
-    $('#content').hide('fast',loadContent);
-    window.location.hash = link.replace('.html', '');
-    function loadContent() {
-     $('#content').load(toLoad,'',showNewContent())
-    }
-    function showNewContent() {
-     $('#content').show('normal',hideLoader());
-    }
-    function hideLoader() {
-     $('#load').fadeOut('normal');
-    }
+function changeStyle(obj) {
+  $('#headernav div a').css({'color' : 'black', 'padding-top' : ''});
+  $('.animationdiv').css({'border-bottom' : ''});
+  obj.css({'color' : '#2375E1', 'padding-top' : '3px'});
+  obj.parent().css({ 'border-bottom' : '3px solid #2375E1'});
 }
-
-$( document ).ajaxComplete(function() {
-  initMap();
-});
