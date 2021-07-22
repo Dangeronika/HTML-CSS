c081ec439befc64d315962a431448d5a465fcb9d
Dangeronika <Panda99230401@yandex.ru>
2021-07-22 19:31:37 +0300

    Add working history API without selected current page

--- js/history.js
+++ js/history.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
 
-  $('#headernav div a').click(function(e){
+  $('#headernav div a').click(function(e) {
       e.preventDefault();
-      changeStyle($(this));
-      href = $(this).attr('href');
+      href = '/' + $(this).attr('href');
       let toLoad = href+' #content';
-      $('#content').hide('fast',loadContent);
+      $('#content').hide('fast', () => loadContent(toLoad));
       history.pushState(null, null, href);
-      function loadContent() {
-      $('#content').load(toLoad,'', () => showNewContent())
-      }
-      function showNewContent() {
-      $('#content').show('normal',hideLoader());
-      if (href == "Map.html") { initMap(); }
-      }
-      function hideLoader() {
-      $('#load').fadeOut('normal');
-      }
   })
 })
 
-function changeStyle(obj) {
-  $('#headernav div a').css({'color' : 'black', 'padding-top' : ''});
-  $('.animationdiv').css({'border-bottom' : ''});
-  obj.css({'color' : '#2375E1', 'padding-top' : '3px'});
-  obj.parent().css({ 'border-bottom' : '3px solid #2375E1'});
+// function changeStyle(str) {
+//   $('#headernav div a').css({'color' : 'black', 'padding-top' : ''});
+//   $('.animationdiv').css({'border-bottom' : ''});
+//   obj.css({'color' : '#2375E1', 'padding-top' : '3px'});
+//   obj.parent().css({ 'border-bottom' : '3px solid #2375E1'});
+// }
+
+function loadContent( road ) {
+  $('#content').load( road, '', () => showNewContent())
 }
+function showNewContent() {
+  $('#content').show('normal', hideLoader());
+
+  if(document.location.pathname == '/Map.html') {
+    initMap();
+  }
+}
+function hideLoader() {
+  $('#load').fadeOut('normal');
+}
+
+$(window).bind('popstate', function(e) {
+
+  $('#content').hide('fast',
+   loadContent(document.location.pathname + ' #content'));
+})
